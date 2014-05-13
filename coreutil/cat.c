130d40a5f5881215a658716be8b1804ebd285f01
ren <code@apotheon.net>
2014-05-13 19:08:49 +0000

    added option handling

--- coreutil/cat.c
+++ coreutil/cat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 void usage() {
 	printf("usage: cat [-huv] [file [...]]\n");
@@ -14,6 +15,17 @@ void version() {
 	printf("cat v0.1\n");
 }
 
+
 int main(int argc, char *argv[]) {
+#define OPT(a) (strcmp(argv[i], a) == 0)
+	for (int i = 1; i < argc; i++) {
+		if (OPT("-h") || OPT("--help")) usage();
+
+		if (OPT("-u") || OPT("--unbuffered")) {
+			printf("Unbuffered use has not yet been implemented.\n");
+		}
+
+		if (OPT("-v") || OPT("--version")) version();
+	}
 	return 0;
 }
