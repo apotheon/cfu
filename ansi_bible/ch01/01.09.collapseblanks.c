24132e0198048976e6aa4db6318d17dff68a6cd2
ren <code@apotheon.net>
2017-02-14 21:56:20 +0000

    add exercise note to 01-09

--- ansi_bible/ch01/01.09.collapseblanks.c
+++ ansi_bible/ch01/01.09.collapseblanks.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+ * 01-09: Print input with any number of adjacent blank characters collapsed
+ *        into only one blank character.
+ */
+
 int main() {
     int c = 0;
     int previous = 0;
