81261265f64de4fe236d95c66bddec12badc0dbf
apotheon <apotheon@noemail.net>
2017-02-24 06:32:34 +0000

    add exercise 01-17
    FossilOrigin-Name: bee0e1fce808ae861e6dba0c631b007aef6d9801

---
+++ ansi_bible/ch01/18.trailingspace.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLENGTH 10000
+
+/*
+ * 01-18: Print each line without trailing blanks or tabs.
+ */
+
+/*
+ * Standard "cheating" disclaimers with regard to exercise assumptions apply.
+ */
+
+int clean_line(char line[], int maxlength);
+
+int main() {
+    int length = 0;
+
+    char line[MAXLENGTH];
+    memset(line, 0, MAXLENGTH);
+
+    while ((length = clean_line(line, MAXLENGTH)) > 0) printf("%s", line);
+
+    return 0;
+}
+
+int clean_line(char line[], int maxlength) {
+    int i = 0;
+    int c = 0;
+
+    while ((c = getchar()) != EOF && c != '\n') {
+        if (i < maxlength - 1) line[i] = c;
+        ++i;
+    }
+
+    if (c == '\n') {
+        for (--i; (line[i] == ' ') || (line[i] == '\t'); --i) line[i] = 0;
+        ++i;
+
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+
+    return i;
+}
