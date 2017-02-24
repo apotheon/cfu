4d83286895a45495a32096bd151b1bcc5c27077c
apotheon <code@apotheon.net>
2017-02-24 11:25:24 +0000

    add exercise 01-21 stub

---
+++ ansi_bible/ch01/21.entab.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define TAB 8
+
+/*
+ * 01-21: Replace spaces with tabs for a preset tab stop column width, and pad
+ * out only as necessary with additional spaces.
+ */
+
+/*
+ * Standard "cheating" disclaimers with regard to exercise assumptions apply.
+ */
+
+int main() {
+    int c = 0;
+    return 0;
+}
