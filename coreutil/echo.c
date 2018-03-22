f1d1086961245c33b1cafc488f69589d43d400f4
apotheon <apotheon@noemail.net>
2018-03-22 13:42:30 +0000

    update to evolved standards of not-as-crappy work
    FossilOrigin-Name: 10e8335511018fb3006cefc0277c64687883a9a1

--- coreutil/echo.c
+++ coreutil/echo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "commonopts.h"
+/*#include "commonopts.h"*/
 
 
 /* begin informational output strings */
@@ -40,37 +40,53 @@ const char *help_text = "mimic - write arguments to standard output\n"
 "\n"
 "     -v\n"
 "     --version\n"
-"             Print version information.\n";
+"             Print version information.\n"
 "\n"
 "     -W <COMMAND>\n"
 "             Print out information specified by <COMMAND>.\n"
-"             (No commands are currently implemented.\n"
+"             (No commands are currently implemented.)\n"
 "\n"
 "     --\n"
 "             Disable further option handling on the command line.\n";
 
 
-const char *license_text = "Copyright 2014 Chad Perrin\n"
+const char *license_text = "Copyright 2016 Chad Perrin, released under the "
+"terms of version 0.4 of\nthe Copyfree Open Innovation License.\n"
 "\n"
-"Permission is hereby granted by the holder(s) of copyright or other le-\n"
-"gal privileges, author(s) or assembler(s), and contributor(s) of this\n"
-"work, to any person who obtains a copy of this work in any form, to re-\n"
-"produce, modify, distribute, publish, sell, sublicense, use, and/or\n"
-"otherwise deal in the licensed material without restriction, provided\n"
-"the following conditions are met:\n"
+"## Terms and Conditions\n"
 "\n"
-"Redistributions, modified or unmodified, in whole or in part, must re-\n"
-"tain applicable copyright and other legal privilege notices, the above\n"
-"license notice, these conditions, and the following disclaimer.\n"
+"Redistributions, modified or unmodified, in whole or in part, must retain\n"
+"applicable copyright or other legal privilege notices, these conditions, "
+"and\nthe following license terms and disclaimer.  Subject to these "
+"conditions, the\nholder(s) of copyright or other legal privileges, author(s) "
+"or assembler(s),\nand contributors of this work hereby grant to any person "
+"who obtains a copy of\nthis work in any form:\n"
 "\n"
-"NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS\n"
-"LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE, IN-\n"
-"CLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS\n"
-"FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE\n"
-"AUTHORS, ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE\n"
-"BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION\n"
-"OF CONTRACT, TORT, OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION\n"
-"WITH THE WORK OR THE USE OF OR OTHER DEALINGS IN THE WORK.\n";
+"1. Permission to reproduce, modify, distribute, publish, sell, sublicense, "
+"use,\nand/or otherwise deal in the licensed material without restriction.\n"
+"\n"
+"2. A perpetual, worldwide, non-exclusive, royalty-free, irrevocable patent\n"
+"license to reproduce, modify, distribute, publish, sell, use, and/or "
+"otherwise\ndeal in the licensed material without restriction, for any and all "
+"patents:\n"
+"\n"
+"    a. Held presently or in the future by each such holder of copyright or\n"
+"    other legal privilege, author or assembler, or contributor, necessarily\n"
+"    infringed by the contributions alone or by combination with the work, of\n"
+"    that privilege holder, author or assembler, or contributor.\n"
+"\n"
+"    b. Necessarily infringed by the work at the time that holder of "
+"copyright\n    or other privilege, author or assembler, or contributor made "
+"any\n    contribution to the work.\n"
+"\n"
+"NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS "
+"LICENSE\nOR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE, "
+"INCLUDING BUT NOT\nLIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR "
+"A PARTICULAR PURPOSE,\nAND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS, "
+"ASSEMBLERS, OR HOLDERS OF\nCOPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR "
+"ANY CLAIM, DAMAGES, OR OTHER\nLIABILITY, WHETHER IN ACTION OF CONTRACT, TORT, "
+"OR OTHERWISE ARISING FROM, OUT\nOF, OR IN CONNECTION WITH THE WORK OR THE USE "
+"OF OR OTHER DEALINGS IN THE WORK.\n";
 
 /* end informational output strings */
 
@@ -79,47 +95,16 @@ const char *license_text = "Copyright 2014 Chad Perrin\n"
 
 int help() {
 	printf("\n%s\n", help_text);
-
 	exit(0);
 }
 
 int license() {
 	printf("\n%s\n", license_text);
-
 	exit(0);
 }
 
 int version() {
 	printf("echo version %s, copyright 2014\n", version_number);
-	puts(
-		"This software may be distributed under the terms of the\n"
-		"Detachable Public License (DPL):"
-		"Permission is hereby granted by the holder(s) "
-		"of copyright or other legal\n"
-		"privileges, author(s) or assembler(s), and "
-		"contributor(s) of this work, to\n"
-		"any person who obtains a copy of this work in any form, to reproduce,\n"
-"modify, distribute, publish, sell, sublicense, use, and/or otherwise deal\n"
-"in the licensed material without restriction, provided the following\n"
-"conditions are met.\n"
-
-Redistributions, modified or unmodified, in whole or in part, must make
-applicable copyright and other legal privilege notices, the above license
-notice, these conditions, and the following disclaimer, freely available to
-recipients on distribution through one of the following means, in descending
-order of preferability: incorporated into the work, provided with the work,
-presented via the medium or method of distribution for the work, or upon
-request by recipients of the work while the work is offered for distribution.
-
-NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS LICENSE
-OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE, INCLUDING BUT NOT
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
-AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS, ASSEMBLERS, OR HOLDERS OF
-COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR ANY CLAIM, DAMAGES, OR OTHER
-LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT, OR OTHERWISE ARISING FROM, OUT
-OF, OR IN CONNECTION WITH THE WORK OR THE USE OF OR OTHER DEALINGS IN THE WORK.
-	);
-
 	exit(0);
 }
 
@@ -130,8 +115,8 @@ void mimic(int newline, char *args[], int start, int end) {
 	if (start > end) {
 		puts("");
 	} else {
-		for (int i = start; i < end; i++) {
-			printf("%s", args[i]);
+		for (int i = start; i < end; ++i) {
+			printf("%s", *(args+i));
 			if ((i+1) < end) printf(" ");
 			else if (newline) puts("");
 		}
@@ -139,8 +124,8 @@ void mimic(int newline, char *args[], int start, int end) {
 }
 
 
-int main(int argc, char *argv[]) {
-#define OPT(a) (strcmp(argv[1], a) == 0)
+int main(int argc, char **argv) {
+#define OPT(a) (strcmp(*(argv + 1), a) == 0)
 	int nflag = 1;
 	int startopt = 2;
 
