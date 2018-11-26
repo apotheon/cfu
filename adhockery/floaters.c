23d99a66f38d697a2ae0faf76d9630d3cfe224e2
apotheon <code@apotheon.net>
2018-11-26 06:56:34 +0000

    add expected output comment to floaters.c

--- adhockery/floaters.c
+++ adhockery/floaters.c
@@ -22,5 +22,23 @@ int main() {
 		puts("type for binary numbers or when explicitly declared in code.\n");
 	}
 
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+	 * When compiled and executed with IEEE 754 as your default floating *
+	 * point type, the program's output should look something like this: *
+	 *                                                                   *
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 *                                                                   *
+	 *   If you expect decimal arithmetic behavior from your IEEE 754    *
+	 *   floating point numbers, you will eventually be disappointed.    *
+	 *                                                                   *
+	 *                0.1 + 0.2 != 0.3000000000000000444                 *
+	 *                            500 != 501                             *
+	 *                                                                   *
+	 *   This is an example of why default floats in a language using    *
+	 *   decimal numerals by default should be decimal floats instead    *
+	 *   of binary floats.  IEEE 754 should only be used as the float    *
+	 *   type for binary numbers or when explicitly declared in code.    *
+	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 	return 0;
 }
