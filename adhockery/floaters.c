53ee092bf1afc23b887b363d2fc736a0c2ab10c1
apotheon <apotheon@noemail.net>
2018-11-26 06:40:15 +0000

    fix dyslexic typo
    FossilOrigin-Name: c0e47cf3efad3bcc3ea620e8b073e92c4d908b03

--- adhockery/floaters.c
+++ adhockery/floaters.c
@@ -12,7 +12,7 @@ int main() {
 	if ((0.1 + 0.2) == 0.3) {
 		puts("Everything is peachy!");
 	} else {
-		puts("\nIf you expect decimal arithmetic behavior from your IEEE 457");
+		puts("\nIf you expect decimal arithmetic behavior from your IEEE 754");
 		puts("floating point numbers, you will eventually be disappointed.\n");
 		printf("             0.1 + 0.2 != %20.19f\n", 0.1 + 0.2);
 		printf("                         %d != %d\n\n", (int)(x * 10), count);
