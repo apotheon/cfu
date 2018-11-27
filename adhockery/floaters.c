917e04df8308564674e52a9ec23229f2faed30f4
apotheon <code@apotheon.net>
2018-11-27 01:13:11 +0000

    adjust floaters.c language to account for existence of IEEE 754 dec floats

--- adhockery/floaters.c
+++ adhockery/floaters.c
@@ -13,32 +13,34 @@ int main() {
 		puts("Everything is peachy!");
 	} else {
 		puts("\nIf you expect decimal arithmetic behavior from your IEEE 754");
-		puts("floating point numbers, you will eventually be disappointed.\n");
+		puts("binary floating points, you will eventually be disappointed.\n");
 		printf("             0.1 + 0.2 != %20.19f\n", 0.1 + 0.2);
 		printf("                         %d != %d\n\n", (int)(x * 10), count);
 		puts("\nThis is an example of why default floats in a language using");
 		puts("decimal numerals by default should be decimal floats instead");
-		puts("of binary floats.  IEEE 754 should only be used as the float");
-		puts("type for binary numbers or when explicitly declared in code.\n");
+		puts("of binary floats.  We shouldn't use IEEE 754 binary floating");
+		puts("points except with binary or when made explicit in our code.\n");
 	}
 
-	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
-	 * When compiled and executed with IEEE 754 as your default floating *
-	 * point type, the program's output should look something like this: *
-	 *                                                                   *
-	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-	 *                                                                   *
-	 *   If you expect decimal arithmetic behavior from your IEEE 754    *
-	 *   floating point numbers, you will eventually be disappointed.    *
-	 *                                                                   *
-	 *                0.1 + 0.2 != 0.3000000000000000444                 *
-	 *                            499 != 500                             *
-	 *                                                                   *
-	 *   This is an example of why default floats in a language using    *
-	 *   decimal numerals by default should be decimal floats instead    *
-	 *   of binary floats.  IEEE 754 should only be used as the float    *
-	 *   type for binary numbers or when explicitly declared in code.    *
-	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+	 *                                                                     *
+	 *  When compiled with IEEE 754 binary floating points, the program's  *
+	 *  output should look something like the following explanation text:  *
+	 *                                                                     *
+	 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	 *                                                                     *
+	 *    If you expect decimal arithmetic behavior from your IEEE 754     *
+	 *    binary floating points, you will eventually be disappointed.     *
+	 *                                                                     *
+	 *                 0.1 + 0.2 != 0.3000000000000000444                  *
+	 *                             499 != 500                              *
+	 *                                                                     *
+	 *    This is an example of why default floats in a language using     *
+	 *    decimal numerals by default should be decimal floats instead     *
+	 *    of binary floats.  We shouldn't use IEEE 754 binary floating     *
+	 *    points except with binary or when made explicit in our code.     *
+	 *                                                                     *
+	\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 	return 0;
 }
