a29c3dd3f3cdc0ac7655d4b6780d297e5807f293
apotheon <apotheon@noemail.net>
2020-04-08 02:26:10 +0000

    add cpstr_bylen() to copystring.c
    FossilOrigin-Name: dfe12ea32b8690430bf82bdcbf60c54caf849449

--- adhockery/copystring.c
+++ adhockery/copystring.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *copystr(char *dest, const char *source);
+int cpstr_bylen(char *dest, int len, const char *source);
 
 int main() {
 	int units = 16;
@@ -9,6 +11,26 @@ int main() {
 	char *arr0 = calloc(units, sizeof(*arr0));
 	printf("copystr:        %s\n", copystr(arr0, "Copy my string."));
 	free(arr0);
+
+	puts("");
+
+	char *arr1 = calloc(units, sizeof(*arr1));
+	printf(
+		"cpstr_bylen:    %i char difference\ncopied text:    %s\n",
+		cpstr_bylen(arr1, (units - 1), "Copy my string."),
+		arr1
+	);
+	free(arr1);
+
+	puts("");
+
+	char *arr2 = calloc(units, sizeof(*arr2));
+	printf(
+		"cpstr_bylen:    %i char difference\ncopied text:    %s\n",
+		cpstr_bylen(arr2, (units - 1), "Copy as much of my string as you can."),
+		arr2
+	);
+	free(arr2);
 
 	return 0;
 }
@@ -22,4 +44,19 @@ char *copystr(char *dest, const char *source) {
 	} while (*(source + i));
 
 	return dest;
+}
+
+int cpstr_bylen(char *dest, int len, const char *source) {
+	int i = 0;
+
+	do {
+		*(dest + i) = *(source + i);
+		++i;
+	} while (*(source + i) && (i < len));
+
+	if (i >= len) {
+		return strlen(dest) - strlen(source);
+	} else {
+		return 0;
+	}
 }
