aaf2e8151f2bc2af2baac7e81913b9902d1759af
apotheon <apotheon@noemail.net>
2020-04-08 00:25:38 +0000

    add copystring.c
    FossilOrigin-Name: 9e7ae174c608b8d9d1b50f6213708414f992c1b1

---
+++ adhockery/copystring.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *copystr(char *dest, const char *source);
+
+int main() {
+	int units = 16;
+	char *arr = calloc(units, sizeof(*arr));
+
+	copystr(arr, "Copy my string.");
+	puts(arr);
+
+	return 0;
+}
+
+char *copystr(char *dest, const char *source) {
+	int i = 0;
+
+	do {
+		*(dest + i) = *(source + i);
+		++i;
+	} while (*(source + i) != '\0');
+
+	return dest;
+}
